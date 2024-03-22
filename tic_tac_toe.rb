5792a0c1adf73789456500707d1b21424611d57a
Samson Wu <160843158+KiyuV@users.noreply.github.com>
2024-03-22 11:25:34 +0200

    fix: fix a typo and linted source code

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,55 +1,56 @@
+# frozen_string_literal: true
+
 class TicTacToe
-  attr_reader :player_1, :player_2
-  
-  def initialize(player_1, player_2)
-    @board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    @current_symbol = "X"
-    @players = [player_1, player_2]
-    self.assign_players
+  attr_reader :player1, :player2
+
+  def initialize(player1, player2)
+    @board = %w[1 2 3 4 5 6 7 8 9]
+    @current_symbol = 'X'
+    @players = [player1, player2]
+    assign_players
   end
 
   def to_s
     puts
     puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-    puts "— — — — —"
+    puts '— — — — —'
     puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-    puts "— — — — —"
+    puts '— — — — —'
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
     puts
   end
 
   def player_input(number)
     @board[number - 1] = @current_symbol
-    @current_symbol == "X" ? @current_symbol = "O" : @current_symbol = "X"
-    self.to_s
+    @current_symbol = @current_symbol == 'X' ? 'O' : 'X'
+    to_s
   end
 
   private
 
   def assign_players
     current_player = @players.sample
-    @player_1 = current_player
+    @player1 = current_player
     @players.delete(current_player)
-    @player_2 = @players[0]
+    @player2 = @players[0]
   end
 end
 
-puts "Welcome to Tic Tac Toe!"
+puts 'Welcome to Tic Tac Toe!'
 
 puts "Please enter Player 1's name"
-player_1 = gets.chomp
+player1 = gets.chomp
 puts "Please enter Player 2's name"
-player_2 = gets.chomp
+player2 = gets.chomp
 
-
-game = TicTacToe.new(player_1, player_2``)
+game = TicTacToe.new(player1, player2)
 game.to_s
 
 current_player = 0
-players = [game.player_1, game.player_2]
-9.times do 
+players = [game.player1, game.player2]
+9.times do
   puts "Your turn, #{players[current_player]}!"
   game.player_input(gets.chomp.to_i)
-  current_player == 0 ? current_player = 1 : current_player = 0
+  current_player = current_player.zero? ? 1 : 0
   puts
 end
